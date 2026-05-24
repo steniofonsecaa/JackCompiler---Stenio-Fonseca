0968561b37536aa4a7c8cab546c4d02c8c87cfd9
Stenio Moraes Fonseca <stenioomoraes12@gmail.com>
2026-05-24 11:13:06 -0300

    Adiçao de um enum para os segmentos de memoria.

--- JackCompiler/Modules/VMWriter.cs
+++ JackCompiler/Modules/VMWriter.cs
@@ -3,5 +3,9 @@ using System.IO;
 
 namespace JackCompiler.Modules
 {
-    
+    // Enum para os segmentos de memória usados na linguagem VM
+    public enum Segment
+    {
+        CONST, ARG, LOCAL, STATIC, THIS, THAT, POINTER, TEMP
+    }
 }
